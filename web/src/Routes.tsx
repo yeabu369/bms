707720bb88081008fb47759d4c6ad97aceadf373
Yeabsra Hailu <yeabsdebe@gmail.com>
2022-05-07 20:25:59 +0300

    protected admin routes

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -13,11 +13,11 @@ import BooksLayout from 'src/layouts/BooksLayout'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/login" page={LoginPage} name="login" />
-      <Route path="/signup" page={SignupPage} name="signup" />
+      <Route path="/admin/login" page={LoginPage} name="login" />
+      <Route path="/admin/signup" page={SignupPage} name="signup" />
       <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
       <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
-      <Set wrap={BooksLayout}>
+      <Set private unauthenticated="login" wrap={BooksLayout}>
         <Route path="/books/new" page={BookNewBookPage} name="newBook" />
         <Route path="/books/{id:Int}/edit" page={BookEditBookPage} name="editBook" />
         <Route path="/books/{id:Int}" page={BookBookPage} name="book" />
